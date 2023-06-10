d09803185a51ff8c3b52279c75c2c28fbc0beb2f
shashank7200 <shashank7200@gmail.com>
2023-06-10 15:48:35 +0530

    abstracted constants from the code

---
+++ constants.js
@@ -0,0 +1,14 @@
+export const API_KEY_ID = 'api_key';
+
+export const LinkOptions = {
+    Download: 'D',
+    Stream: 'S',
+    Open: 'O',
+};
+
+export const cliOptions = {
+    config: ['--config', '-c', 'config'],
+    discover: ['--discover', '-d', 'discover'],
+    version: ['--version', '-v'],
+    help: ['--help', '-h'],
+};
